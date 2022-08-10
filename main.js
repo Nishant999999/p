24838202368fab8909e66c9448267bdb02f81d12
Nishant999999 <98709074+Nishant999999@users.noreply.github.com>
2022-08-09 22:40:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function addRoom()
+window.location="";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNOPGEO_MvvpG2PfdBf-d5QkaIjsh7OCM",
+    authDomain: "c-94-project-beda0.firebaseapp.com",
+    projectId: "c-94-project-beda0",
+    storageBucket: "c-94-project-beda0.appspot.com",
+    messagingSenderId: "495763553035",
+    appId: "1:495763553035:web:aa1d6fd6a201f8c33d800e"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
